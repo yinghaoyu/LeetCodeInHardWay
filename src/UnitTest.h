12ba760d4eafb0eebf2234c44f42973855002706
rain <20520@126.com>
2022-09-14 11:25:10 +0800

    fix double compare

--- src/UnitTest.h
+++ src/UnitTest.h
@@ -21,7 +21,7 @@ static int test_pass = 0;
 
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 
-#define EXPECT_EQ_DOUBLE(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%.17g")
+#define EXPECT_EQ_DOUBLE(expect, actual) EXPECT_EQ_BASE((expect) - (actual) < 1e-5, expect, actual, "%.17g")
 
 #define EXPECT_TRUE(actual) EXPECT_EQ_BASE((actual) != 0, "true", "false", "%s")
 
