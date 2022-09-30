76db6b5baa6b1a841702d189fcdc1abbbbf9a04f
rain <20520@126.com>
2022-09-30 15:45:24 +0800

    fix list

--- src/Problem_02.01_removeDuplicateNodes.cc
+++ src/Problem_02.01_removeDuplicateNodes.cc
@@ -61,19 +61,31 @@ void testRemoveDuplicateNodes()
   ListNode *x4 = new ListNode(3);
   ListNode *x5 = new ListNode(2);
   ListNode *x6 = new ListNode(1);
+  x1->next = x2;
+  x2->next = x3;
+  x3->next = x4;
+  x4->next = x5;
+  x5->next = x6;
 
   ListNode *x7 = new ListNode(1);
   ListNode *x8 = new ListNode(2);
   ListNode *x9 = new ListNode(3);
+  x7->next = x8;
+  x8->next = x9;
 
   ListNode *y1 = new ListNode(1);
   ListNode *y2 = new ListNode(1);
   ListNode *y3 = new ListNode(1);
   ListNode *y4 = new ListNode(1);
   ListNode *y5 = new ListNode(2);
+  y1->next = y2;
+  y2->next = y3;
+  y3->next = y4;
+  y4->next = y5;
 
   ListNode *y6 = new ListNode(1);
   ListNode *y7 = new ListNode(2);
+  y6->next = y7;
 
   EXPECT_TRUE(isListEqual(x7, s.removeDuplicateNodes(x1)));
   EXPECT_TRUE(isListEqual(y6, s.removeDuplicateNodes(y1)));
