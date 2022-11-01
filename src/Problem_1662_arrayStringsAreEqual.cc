d7ab83e21f1a38f3dc965d1b17a509d116e5ce33
rain <20520@126.com>
2022-11-01 14:26:24 +0800

    add more policy in 1662

--- src/Problem_1662_arrayStringsAreEqual.cc
+++ src/Problem_1662_arrayStringsAreEqual.cc
@@ -11,15 +11,48 @@ using namespace std;
 class Solution
 {
  public:
-  bool arrayStringsAreEqual(vector<string> &word1, vector<string> &word2)
+  bool arrayStringsAreEqual1(vector<string> &word1, vector<string> &word2)
   {
     auto p1 = std::accumulate(word1.begin(), word1.end(), string(""), [](string &s1, string &s2) { return s1 + s2; });
     auto p2 = std::accumulate(word2.begin(), word2.end(), string(""), [](string &s1, string &s2) { return s1 + s2; });
+    return p1 == p2;
+  }
+
+  bool arrayStringsAreEqual2(vector<string> &word1, vector<string> &word2)
+  {
     string p3;
     string p4;
     std::for_each(word1.begin(), word1.end(), [&](const string &s) { p3 += s; });
     std::for_each(word2.begin(), word2.end(), [&](const string &s) { p4 += s; });
-    return p1 == p2 && p3 == p4;
+    return p3 == p4;
+  }
+
+  bool arrayStringsAreEqual3(vector<string> &word1, vector<string> &word2)
+  {
+    int i = 0;
+    int j = 0;
+    int p = 0;
+    int q = 0;
+    int n = word1.size();
+    int m = word2.size();
+    while (i < n && j < m)
+    {
+      if (word1[i][p++] != word2[j][q++])
+      {
+        return false;
+      }
+      if (p == word1[i].length())
+      {
+        i++;
+        p = 0;
+      }
+      if (q == word2[j].length())
+      {
+        j++;
+        q = 0;
+      }
+    }
+    return i == n && j == m;
   }
 };
 
@@ -32,9 +65,15 @@ void testArrayStringsAreEqual()
   vector<string> n4 = {"ab", "c"};
   vector<string> n5 = {"abc", "d", "defg"};
   vector<string> n6 = {"abcddefg"};
-  EXPECT_TRUE(s.arrayStringsAreEqual(n1, n2));
-  EXPECT_FALSE(s.arrayStringsAreEqual(n3, n4));
-  EXPECT_TRUE(s.arrayStringsAreEqual(n5, n6));
+  EXPECT_TRUE(s.arrayStringsAreEqual1(n1, n2));
+  EXPECT_FALSE(s.arrayStringsAreEqual1(n3, n4));
+  EXPECT_TRUE(s.arrayStringsAreEqual1(n5, n6));
+  EXPECT_TRUE(s.arrayStringsAreEqual2(n1, n2));
+  EXPECT_FALSE(s.arrayStringsAreEqual2(n3, n4));
+  EXPECT_TRUE(s.arrayStringsAreEqual2(n5, n6));
+  EXPECT_TRUE(s.arrayStringsAreEqual3(n1, n2));
+  EXPECT_FALSE(s.arrayStringsAreEqual3(n3, n4));
+  EXPECT_TRUE(s.arrayStringsAreEqual3(n5, n6));
   EXPECT_SUMMARY;
 }
 
