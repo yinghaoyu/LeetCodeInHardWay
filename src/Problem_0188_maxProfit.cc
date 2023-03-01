b95c16880c2bdc9c3309f2f0e44d7d6c09111c38
rain <20520@126.com>
2023-03-01 08:32:09 +0800

    fix 0188

--- src/Problem_0188_maxProfit.cc
+++ src/Problem_0188_maxProfit.cc
@@ -37,16 +37,26 @@ class Solution
     int n = prices.size();
     if (k >= n / 2)
     {
+      // 根据 122 题的结论：
+      // 如果数组元素个数为N，则最多执行N/2次交易就可以抓取所有的上升段的值
       return allTrans(prices);
     }
+    // dp[i][j]表示[0...i]范围内交易j次获得的最大收益是多少
     vector<vector<int>> dp(n, vector<int>(k + 1));
+    // 第一行的值表示数组[0..0]范围内，交易若干次的最大收益，显然，都是0
+    // 第一列的值表示数组[0...i]范围内，交易0次获得的最大收益，显然，也都是0
     for (int i = 1; i < n; i++)
     {
       for (int j = 1; j < k + 1; j++)
       {
+        // i位置不参与交易
         dp[i][j] = dp[i - 1][j];
+        // i位置如果参与交易，那么i位置一定是最后一次的卖出时机
+        // 枚举最后一次的买入时机
         for (int p = 0; p <= i; p++)
         {
+          // 最后一次买入的时机在i位置
+          // FIXME: 这里枚举可以优化
           dp[i][j] = std::max(dp[p][j - 1] + prices[i] - prices[p], dp[i][j]);
         }
       }
@@ -64,6 +74,8 @@ class Solution
     return ans;
   }
 
+  // 枚举优化
+  // https://www.cnblogs.com/greyzeng/p/16182420.html
   int maxProfit3(int k, vector<int> &prices)
   {
     if (prices.size() == 0)
