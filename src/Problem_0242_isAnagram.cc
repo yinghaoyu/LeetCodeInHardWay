6252403377b260b7dcbda6f423c6fad4eceb3cce
rain <20520@126.com>
2023-03-05 20:10:48 +0800

    add 0242

---
+++ src/Problem_0242_isAnagram.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+#include "UnitTest.h"
+
+using namespace std;
+
+class Solution
+{
+ public:
+  bool isAnagram(string s, string t)
+  {
+    vector<int> cnt(256);
+    for (auto &c : s)
+    {
+      cnt[c]++;
+    }
+    for (auto &c : t)
+    {
+      if (--cnt[c] < 0)
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+void testIsAnagram()
+{
+  Solution s;
+  EXPECT_TRUE(s.isAnagram("anagram", "nagaram"));
+  EXPECT_FALSE(s.isAnagram("rat", "car"));
+  EXPECT_SUMMARY;
+}
+
+int main()
+{
+  testIsAnagram();
+  return 0;
+}
