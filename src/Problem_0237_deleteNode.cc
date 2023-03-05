a68f33f2a8161c771344a57c4219fbc081f2f7e8
rain <20520@126.com>
2023-03-05 09:58:05 +0800

    add 0237

---
+++ src/Problem_0237_deleteNode.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+ public:
+  void deleteNode(ListNode *node)
+  {
+    if (node == nullptr)
+    {
+      return;
+    }
+    if (node->next == nullptr)
+    {
+      node = nullptr;
+      return;
+    }
+    node->val = node->next->val;
+    node->next = node->next->next;
+  }
+};
