8b177463161da6c3365e159754b0a4aa5a0c637f
rain <20520@126.com>
2023-03-22 09:24:31 +0800

    add 0437

--- README.md
+++ README.md
@@ -170,6 +170,7 @@ LeetCode Solution In Hard Way
 |0543|[Diameter of Binary Tree](https://leetcode.cn/problems/diameter-of-binary-tree/) | [C++](./src/Problem_0543_diameterOfBinaryTree.cc)|Easy|
 |0481|[Magical String](https://leetcode.cn/problems/magical-string/) | [C++](./src/Problem_0481_magicalString.cc)|Medium|
 |0454|[4Sum II](https://leetcode.cn/problems/4sum-ii/) | [C++](./src/Problem_0454_fourSumCount.cc)|Medium|
+|0437|[Path Sum III](https://leetcode.cn/problems/path-sum-iii/) | [C++](./src/Problem_0437_pathSum.cc)|Medium|
 |0412|[Fizz Buzz](https://leetcode.cn/problems/fizz-buzz/) | [C++](./src/Problem_0412_fizzBuzz.cc)|Easy|
 |0406|[Queue Reconstruction by Height](https://leetcode.cn/problems/queue-reconstruction-by-height/) | [C++](./src/Problem_0406_reconstructQueue.cc)|Medium|
 |0395|[Longest Substring with At Least K Repeating Characters](https://leetcode.cn/problems/longest-substring-with-at-least-k-repeating-characters/) | [C++](./src/Problem_0395_longestSubstring.cc)|Medium|
---
+++ src/Problem_0437_pathSum.cc
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+#include "UnitTest.h"
+
+using namespace std;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+ public:
+  int pathSum(TreeNode *root, int targetSum)
+  {
+    unordered_map<long long, int> map;
+    // 这是因为任何节点本身也可以形成一个路径（长度为1的路径）。
+    // 如果某个节点的值就为target，那么它本身就是一个解。
+    // 前缀和0正好可以与它形成这个解。
+    // 对任何节点而言，本身就是解最多只能有一个，所以一开始empale(0, 1)。
+    // 相当于给所有节点一个可单独形成合法路径的机会。
+    map.emplace(0L, 1);
+    return process(root, targetSum, 0, map);
+  }
+
+  // 前缀和
+  int process(TreeNode *x, int targetSum, long long pre, unordered_map<long long, int> &map)
+  {
+    if (x == nullptr)
+    {
+      return 0;
+    }
+    long long all = pre + x->val;
+    int ans = 0;
+    if (map.count(all - targetSum))
+    {
+      // 说明路径上存在和为 targetSum
+      ans = map.at(all - targetSum);
+    }
+    if (!map.count(all))
+    {
+      map.emplace(all, 1);
+    }
+    else
+    {
+      map[all]++;
+    }
+    ans += process(x->left, targetSum, all, map);
+    ans += process(x->right, targetSum, all, map);
+    // 回溯，保证map里的值一定是连续的节点生成的
+    if (map.at(all) == 1)
+    {
+      map.erase(all);
+    }
+    else
+    {
+      map[all]--;
+    }
+    return ans;
+  }
+};
+
+void testPathSum()
+{
+  Solution s;
+  TreeNode *x9 = new TreeNode(1, nullptr, nullptr);
+  TreeNode *x8 = new TreeNode(-2, nullptr, nullptr);
+  TreeNode *x7 = new TreeNode(3, nullptr, nullptr);
+  TreeNode *x6 = new TreeNode(11, nullptr, nullptr);
+  TreeNode *x5 = new TreeNode(2, nullptr, x9);
+  TreeNode *x4 = new TreeNode(3, x7, x8);
+  TreeNode *x3 = new TreeNode(-3, nullptr, x6);
+  TreeNode *x2 = new TreeNode(5, x4, x5);
+  TreeNode *x1 = new TreeNode(10, x2, x3);
+  EXPECT_EQ_INT(3, s.pathSum(x1, 8));
+  EXPECT_SUMMARY;
+}
+
+int main()
+{
+  testPathSum();
+  return 0;
+}
