a13945c4a600182f1636b018aacfe62084c28e41
rain <20520@126.com>
2023-11-29 18:59:42 +0800

    add description

--- src/Problem_0201_rangeBitwiseAnd.cc
+++ src/Problem_0201_rangeBitwiseAnd.cc
@@ -12,6 +12,13 @@ class Solution
  public:
   int rangeBitwiseAnd(int left, int right)
   {
+    // 假设 right 为 0101001101
+    // 如果 left < right - lowBit(right)
+    // 那么必然存在值 0101001100 会把 0101001101 最左的 1 消掉
+    // 这时 right 取 0101001100
+    // 如果 left < right - lowBit(right)
+    // 那么必然存在值 0101001011 会把 0101001100 最左的 1 消掉
+    // 一直迭代下去
     while (left < right)
     {
       right -= lowBit(right);
