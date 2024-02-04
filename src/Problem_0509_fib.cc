8070c1ded140bfc2c933183c509540ff2fdbf990
rain <20520@126.com>
2024-02-04 10:25:27 +0800

    fix 0509

--- src/Problem_0509_fib.cc
+++ src/Problem_0509_fib.cc
@@ -91,6 +91,61 @@ class Solution
     return last;
   }
 
-  // TODO: 矩阵快速幂
-  int fib5(int n) { return 0; }
+  // 最优解，矩阵快速幂，O(log(n))
+  int fib5(int n)
+  {
+    if (n == 0)
+    {
+      return 0;
+    }
+    if (n == 1)
+    {
+      return 1;
+    }
+    vector<vector<int>> start = {{1, 0}};
+    vector<vector<int>> base = {{1, 1}, {1, 0}};
+    vector<vector<int>> ans = multiply(start, power(base, n - 1));
+    return ans[0][0];
+  }
+
+  // 矩阵相乘
+  // a的列数一定要等于b的行数
+  vector<vector<int>> multiply(vector<vector<int>>& a, vector<vector<int>> b)
+  {
+    int n = a.size();
+    int m = b[0].size();
+    int k = a[0].size();
+    vector<vector<int>> ans(n, vector<int>(m));
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < m; j++)
+      {
+        for (int c = 0; c < k; c++)
+        {
+          ans[i][j] += a[i][c] * b[c][j];
+        }
+      }
+    }
+    return ans;
+  }
+
+  // 矩阵快速幂
+  vector<vector<int>> power(vector<vector<int>>& m, int p)
+  {
+    int n = m.size();
+    vector<vector<int>> ans(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+    {
+      ans[i][i] = 1;
+    }
+    for (; p != 0; p >>= 1)
+    {
+      if (p & 1)
+      {
+        ans = multiply(ans, m);
+      }
+      m = multiply(m, m);
+    }
+    return ans;
+  }
 };
