f55f15cedb368393c3b3b41c9ad0bfd761547bb9
rain <20520@126.com>
2024-02-22 10:53:45 +0800

    enhance 0102

--- src/Problem_0102_levelOrder.cc
+++ src/Problem_0102_levelOrder.cc
@@ -9,33 +9,33 @@ using namespace std;
 struct TreeNode
 {
   int val;
-  TreeNode *left;
-  TreeNode *right;
+  TreeNode* left;
+  TreeNode* right;
   TreeNode() : val(0), left(nullptr), right(nullptr) {}
   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
 
 class Solution
 {
  public:
-  vector<vector<int>> levelOrder(TreeNode *root)
+  vector<vector<int>> levelOrder(TreeNode* root)
   {
     vector<vector<int>> ans;
     if (root == nullptr)
     {
       return ans;
     }
-    queue<TreeNode *> q;
+    queue<TreeNode*> q;
     q.push(root);
     int n = 0;
     while (!q.empty())
     {
       n = q.size();
-      vector<int> tmp;
+      vector<int> nodes;
       for (int i = 0; i < n; i++)
       {
-        TreeNode *cur = q.front();
+        TreeNode* cur = q.front();
         q.pop();
 
         if (cur->left != nullptr)
@@ -46,9 +46,9 @@ class Solution
         {
           q.push(cur->right);
         }
-        tmp.push_back(cur->val);
+        nodes.push_back(cur->val);
       }
-      ans.emplace_back(tmp);
+      ans.emplace_back(nodes);
     }
     return ans;
   }
@@ -57,17 +57,17 @@ class Solution
 void testLevelOrder()
 {
   Solution s;
-  TreeNode *x5 = new TreeNode(7, nullptr, nullptr);
-  TreeNode *x4 = new TreeNode(15, nullptr, nullptr);
-  TreeNode *x3 = new TreeNode(20, x4, x5);
-  TreeNode *x2 = new TreeNode(9, nullptr, nullptr);
-  TreeNode *x1 = new TreeNode(3, x2, x3);
+  TreeNode* x5 = new TreeNode(7, nullptr, nullptr);
+  TreeNode* x4 = new TreeNode(15, nullptr, nullptr);
+  TreeNode* x3 = new TreeNode(20, x4, x5);
+  TreeNode* x2 = new TreeNode(9, nullptr, nullptr);
+  TreeNode* x1 = new TreeNode(3, x2, x3);
   vector<vector<int>> o1 = {{3}, {9, 20}, {15, 7}};
 
-  TreeNode *y = new TreeNode(1, nullptr, nullptr);
+  TreeNode* y = new TreeNode(1, nullptr, nullptr);
   vector<vector<int>> o2 = {{1}};
 
-  TreeNode *z = nullptr;
+  TreeNode* z = nullptr;
   vector<vector<int>> o3;
 
   EXPECT_TRUE(o1 == s.levelOrder(x1));
