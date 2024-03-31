b96391882fad638eaaccdfc6c31d1430138878f7
rain <20520@126.com>
2024-03-31 19:07:27 +0800

    add link in 0691

--- src/Problem_0691_minStickers.cc
+++ src/Problem_0691_minStickers.cc
@@ -7,6 +7,7 @@
 using namespace std;
 
 // TODO: figure it out.
+// @sa https://www.bilibili.com/video/BV1Dw411w7P5/
 class Solution
 {
  public:
@@ -16,15 +17,20 @@ class Solution
     unordered_set<string> visited;
     for (auto& str : stickers)
     {
+      // 贴纸排序不影响结果
       std::sort(str.begin(), str.end());
+      // 建图，当前贴纸能搞定多少字符
       for (int i = 0; i < str.length(); i++)
       {
+        // 过滤重复
         if (i == 0 || str[i] != str[i - 1])
         {
+          // graph[c] 表示能搞定字符 c 的贴纸
           graph[str[i] - 'a'].push_back(str);
         }
       }
     }
+    // 排序不影响结果
     std::sort(target.begin(), target.end());
     visited.emplace(target);
     queue<string> que;
@@ -38,11 +44,17 @@ class Solution
       {
         string cur = que.front();
         que.pop();
+        // 检查哪些贴纸可以搞定首字符
         for (auto& s : graph[cur[0] - 'a'])
         {
+          // cur = "aabccff"
+          // s = "accccfkk"
+          // next = "abf"
+          // 用 s 贴纸搞定 cur 里的字符，还剩余的字符next
           string next = getNext(cur, s);
           if (next.empty())
           {
+            // 目标达成
             return level;
           }
           else if (!visited.count(next))
@@ -57,6 +69,9 @@ class Solution
     return -1;
   }
 
+  // t = "aabccff"
+  // s = "accccfkk"
+  // next = "abf"
   string getNext(string& t, string& s)
   {
     string ans;
