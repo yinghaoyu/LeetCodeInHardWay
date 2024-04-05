1c6bb350de4de9cf53467274114ad5ca6b3facaf
rain <20520@126.com>
2024-04-05 23:32:18 +0800

    enhance 1755

--- src/Problem_1755_minAbsDifference.cc
+++ src/Problem_1755_minAbsDifference.cc
@@ -3,8 +3,9 @@
 
 using namespace std;
 
-// TODO: figure it out.
 // @sa https://www.bilibili.com/video/BV11w411y79P/ Code03
+// 由于 -1e7 <= num[i] <= 1e7，动态规划开辟不了这么大的数组空间
+// 同时全量样本递归展开超时，这时候就需要双向广搜解决
 class Solution
 {
  public:
@@ -41,15 +42,19 @@ class Solution
     std::sort(nums.begin(), nums.end());
     vector<int> lsum;
     vector<int> rsum;
+    // 左部分展开
     collect(nums, 0, n >> 1, 0, lsum);
+    // 右部分展开
     collect(nums, n >> 1, n, 0, rsum);
     std::sort(lsum.begin(), lsum.end());
     std::sort(rsum.begin(), rsum.end());
     int ans = std::abs(goal);
+    // 滑动窗口
     for (int i = 0, j = rsum.size() - 1; i < lsum.size(); i++)
     {
       while (j > 0 && std::abs(goal - lsum[i] - rsum[j - 1]) <= std::abs(goal - lsum[i] - rsum[j]))
       {
+        // 如果滑动后值更小
         j--;
       }
       ans = std::min(ans, std::abs(goal - lsum[i] - rsum[j]));
@@ -65,6 +70,7 @@ class Solution
     }
     else
     {
+      // 前面对 nums 数组排序的作用，在这里剪枝
       // nums[i.....]这一组，相同的数字有几个
       int j = i + 1;
       while (j < e && nums[j] == nums[i])
@@ -73,6 +79,7 @@ class Solution
       }
       // nums[ 1 1 1 1 1 2....
       //       i         j
+      // 这个 for 循环比二叉展开快
       for (int k = 0; k <= j - i; k++)
       {
         // k = 0个
