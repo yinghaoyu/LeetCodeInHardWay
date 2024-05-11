fc9112645824d9d7a716612205786427a964f674
rain <20520@126.com>
2024-05-11 10:58:30 +0800

    fix 0169

--- src/Problem_0169_majorityElement.cc
+++ src/Problem_0169_majorityElement.cc
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 
 #include "UnitTest.h"
@@ -8,23 +7,38 @@ using namespace std;
 class Solution
 {
  public:
-  int majorityElement(vector<int> &nums)
+  int majorityElement(vector<int>& nums)
   {
-    int blood = 0;
-    int solider = 0;
-    for (auto &num : nums)
+    int hp = 0;
+    int candidate = 0;
+    for (auto& num : nums)
     {
-      if (blood == 0)
+      if (hp == 0)
       {
-        solider = num;
-        blood++;
+        candidate = num;
+        hp++;
       }
       else
       {
-        blood += num == solider ? 1 : -1;
+        hp += num == candidate ? 1 : -1;
       }
     }
-    return solider;
+    // 下面这代码不能忽略
+    // {2, 3, 2, 3} 这种是不存在出现次数大于 n/2 的元素的
+    if (hp == 0)
+    {
+      return -1;
+    }
+    // {1, 2, 3, 1, 2, 3, 1} 这种情况 hp > 0，但是 1 的出现次数显然没有大于 7/2 = 3 个
+    hp = 0;
+    for (auto& num : nums)
+    {
+      if (num == candidate)
+      {
+        hp++;
+      }
+    }
+    return hp > nums.size() / 2 ? candidate : -1;
   }
 };
 
