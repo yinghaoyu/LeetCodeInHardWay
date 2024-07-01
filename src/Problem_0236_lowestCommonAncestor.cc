8e71646c64746ad70c3883c67be69dc209a8603f
rain <20520@126.com>
2024-07-01 16:38:43 +0800

    enhance 0236

--- src/Problem_0236_lowestCommonAncestor.cc
+++ src/Problem_0236_lowestCommonAncestor.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ratio>
 
 #include "UnitTest.h"
 
@@ -94,6 +95,120 @@ class Solution
   TreeNode* lowestCommonAncestor2(TreeNode* root, TreeNode* p, TreeNode* q)
   {
     return dfs(root, p, q);
+  }
+
+  // TODO: figure it out.
+  // Morris
+  TreeNode* lowestCommonAncestor3(TreeNode* root, TreeNode* p, TreeNode* q)
+  {
+    if (preOrder(p->left, p, q) != nullptr || preOrder(p->right, p, q))
+    {
+      return p;
+    }
+    if (preOrder(q->left, p, q) != nullptr || preOrder(q->right, p, q))
+    {
+      return q;
+    }
+    TreeNode* left = preOrder(root, p, q);
+    TreeNode* cur = root;
+    TreeNode* mostRight = nullptr;
+    TreeNode* lca = nullptr;
+    while (cur != nullptr)
+    {
+      mostRight = cur->left;
+      if (mostRight != nullptr)
+      {
+        while (mostRight->right != nullptr && mostRight->right != cur)
+        {
+          mostRight = mostRight->right;
+        }
+        if (mostRight->right == nullptr)
+        {
+          mostRight->right = cur;
+          cur = cur->left;
+          continue;
+        }
+        else
+        {
+          mostRight->right = nullptr;
+          if (lca == nullptr)
+          {
+            // 检查left是否在cur左树的右边界上
+            if (rightCheck(cur->left, left))
+            {
+              if (preOrder(left->right, p, q) != nullptr)
+              {
+                lca = left;
+              }
+              left = cur;
+              // 为什么此时检查的是left而不是cur
+              // 因为cur右树上的某些右指针可能没有恢复回来
+              // 需要等右指针恢复回来之后检查才不出错
+              // 所以此时检查的是left而不是cur
+            }
+          }
+        }
+      }
+      cur = cur->right;
+    }
+    // 如果morris遍历结束了还没有收集到答案
+    // 此时最后一个left还没有验证，它一定是答案
+    return lca != nullptr ? lca : left;
+  }
+
+  bool rightCheck(TreeNode* head, TreeNode* target)
+  {
+    while (head != nullptr)
+    {
+      if (head == target)
+      {
+        return true;
+      }
+      head = head->right;
+    }
+    return false;
+  }
+
+  // 以head为头的树进行先序遍历，o1和o2谁先被找到就返回谁
+  TreeNode* preOrder(TreeNode* head, TreeNode* o1, TreeNode* o2)
+  {
+    TreeNode* cur = head;
+    TreeNode* mostRight = nullptr;
+    TreeNode* ans = nullptr;
+    while (cur != nullptr)
+    {
+      mostRight = cur->left;
+      if (mostRight != nullptr)
+      {
+        while (mostRight->right != nullptr && mostRight->right != cur)
+        {
+          mostRight = mostRight->right;
+        }
+        if (mostRight->right == nullptr)
+        {
+          if (ans == nullptr && (cur == o1 || cur == o2))
+          {
+            ans = cur;
+          }
+          mostRight->right = cur;
+          cur = cur->left;
+          continue;
+        }
+        else
+        {
+          mostRight->right = nullptr;
+        }
+      }
+      else
+      {
+        if (ans == nullptr && (cur == o1 || cur == o2))
+        {
+          ans = cur;
+        }
+      }
+      cur = cur->right;
+    }
+    return ans;
   }
 };
 
