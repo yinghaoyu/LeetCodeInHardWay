a6ea0c09cf5e457609e7d4bc7b0c3a3dd1c0bb9d
rain <20520@126.com>
2024-12-11 21:33:22 +0800

    enhance 1851

--- src/Problem_1851_minInterval.cc
+++ src/Problem_1851_minInterval.cc
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <iostream>
 #include <queue>
 #include <vector>
 
@@ -7,40 +6,59 @@
 
 using namespace std;
 
+// 扫描线原理
 class Solution
 {
  public:
-  vector<int> minInterval(vector<vector<int>> &intervals, vector<int> &queries)
+  vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries)
   {
     vector<int> idxs(queries.size());
     for (int i = 0; i < queries.size(); i++)
     {
       idxs[i] = i;
     }
+
     // 索引按 queries[i]  从小到大排序
-    std::sort(idxs.begin(), idxs.end(), [&](int i, int j) -> bool { return queries[i] < queries[j]; });
-    // 区间按 left 从小到大排序
-    std::sort(intervals.begin(), intervals.end(), [](vector<int> &l, vector<int> &r) { return l[0] < r[0]; });
+    // 这样遍历idxs时，就有谁 queries[i] 小，谁先处理，符合扫描线，在x轴从左到右
+    // 使用idxs数组是为了保留编号，方便填答案
+    std::sort(idxs.begin(), idxs.end(),
+              [&](int i, int j) -> bool { return queries[i] < queries[j]; });
+
+    // 区间按开始从小到大排序
+    std::sort(intervals.begin(), intervals.end(),
+              [](vector<int>& l, vector<int>& r) { return l[0] < r[0]; });
+
     // 大根堆
     priority_queue<vector<int>> que;
+
+    // 初始化为-1
     vector<int> ans(queries.size(), -1);
     int i = 0;
-    for (auto &idx : idxs)
+
+    // 从小到大依次遍历 queries[i]
+    for (auto& idx : idxs)
     {
+      // 由于区间按开始从小到大排序，那么先检查这个区间是否包含当前的扫描线 x = queries[idx]
+      // 包含这个扫描线的区间全部进堆
       while (i < intervals.size() && intervals[i][0] <= queries[idx])
       {
         int len = intervals[i][1] - intervals[i][0] + 1;
-        // 堆顶 -len 最大， 对应 len 最小
+        // 取负号，是为了让大根堆达到小根堆的效果，简化C++代码
+        // 最短的区间在堆顶
         que.push({-len, intervals[i][0], intervals[i][1]});
         i++;
       }
       while (!que.empty() && que.top()[2] < queries[idx])
       {
-        // 淘汰范围过小的区间
+        // 当前扫描线 x = queries[idx] 在这些区间的右边，说明这些区间都已经过期了
+        // 注意，这里的-que.top()[0]是最小的，而que.top()[2]不一定是最小的
+        // 可能符合条件的非堆顶区间(脏数据)在这轮没删除，也没关系
+        // 在后面一定会被删除
         que.pop();
       }
       if (!que.empty())
       {
+        // 存在区间，就把区间最小的左端点，填答案
         ans[idx] = -que.top()[0];
       }
     }
