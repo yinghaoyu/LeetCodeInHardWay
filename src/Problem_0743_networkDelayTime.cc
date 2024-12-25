bfccddf602fe9446906a274a5be87d33543d4de3
rain <20520@126.com>
2024-12-25 17:52:20 +0800

    enhance 0743

--- src/Problem_0743_networkDelayTime.cc
+++ src/Problem_0743_networkDelayTime.cc
@@ -66,4 +66,146 @@ class Solution
     }
     return ans;
   }
+
+  // 最优解
+  // 链式前向星+反向索引堆的实现
+  int networkDelayTime2(vector<vector<int>>& times, int n, int s)
+  {
+    build(n);
+    for (auto& edge : times)
+    {
+      addEdge(edge[0], edge[1], edge[2]);
+    }
+    addOrUpdateOrIgnore(s, 0);
+    while (!isEmpty())
+    {
+      int u = pop();
+      for (int ei = head[u]; ei > 0; ei = next[ei])
+      {
+        addOrUpdateOrIgnore(to[ei], distance[u] + weight[ei]);
+      }
+    }
+    int ans = INT32_MIN;
+    for (int i = 1; i <= n; i++)
+    {
+      if (distance[i] == INT32_MAX)
+      {
+        return -1;
+      }
+      ans = std::max(ans, distance[i]);
+    }
+    return ans;
+  }
+
+  static const int MAXN = 101;
+
+  static const int MAXM = 6001;
+
+  // 链式前向星
+  vector<int> head = vector<int>(MAXN);
+
+  vector<int> next = vector<int>(MAXM);
+
+  vector<int> to = vector<int>(MAXM);
+
+  vector<int> weight = vector<int>(MAXM);
+
+  int cnt;
+
+  // 反向索引堆
+  vector<int> heap = vector<int>(MAXN);
+
+  // where[v] = -1，表示v这个节点，从来没有进入过堆
+  // where[v] = -2，表示v这个节点，已经弹出过了
+  // where[v] = i(>=0)，表示v这个节点，在堆上的i位置
+  vector<int> where = vector<int>(MAXN);
+
+  int heapSize;
+
+  vector<int> distance = vector<int>(MAXN);
+
+  void build(int n)
+  {
+    cnt = 1;
+    heapSize = 0;
+    std::fill(head.begin() + 1, head.begin() + n + 1, 0);
+    std::fill(where.begin() + 1, where.begin() + n + 1, -1);
+    std::fill(distance.begin() + 1, distance.begin() + n + 1, INT32_MAX);
+  }
+
+  // 链式前向星建图
+  void addEdge(int u, int v, int w)
+  {
+    next[cnt] = head[u];
+    to[cnt] = v;
+    weight[cnt] = w;
+    head[u] = cnt++;
+  }
+
+  void addOrUpdateOrIgnore(int v, int c)
+  {
+    if (where[v] == -1)
+    {
+      heap[heapSize] = v;
+      where[v] = heapSize++;
+      distance[v] = c;
+      heapInsert(where[v]);
+    }
+    else if (where[v] >= 0)
+    {
+      distance[v] = std::min(distance[v], c);
+      heapInsert(where[v]);
+    }
+  }
+
+  // 反向索引堆的精髓就在于 heapInsert
+  // 如果是小根堆，只允许修改的值 <= 当前的值，
+  // 如果是大根堆，只允许修改的值 >= 当前的值，
+  // 直接从where[i]找i在堆的位置，然后执行 heapInsert
+  // 这样只需要局部向上调整堆，而不是 heapify
+  void heapInsert(int i)
+  {
+    while (distance[heap[i]] < distance[heap[(i - 1) / 2]])
+    {
+      swap(i, (i - 1) / 2);
+      i = (i - 1) / 2;
+    }
+  }
+
+  int pop()
+  {
+    int ans = heap[0];
+    swap(0, --heapSize);
+    heapify(0);
+    where[ans] = -2;
+    return ans;
+  }
+
+  void heapify(int i)
+  {
+    int l = i * 2 + 1;
+    while (l < heapSize)
+    {
+      int best = l + 1 < heapSize && distance[heap[l + 1]] < distance[heap[l]] ? l + 1 : l;
+      best = distance[heap[best]] < distance[heap[i]] ? best : i;
+      if (best == i)
+      {
+        break;
+      }
+      swap(best, i);
+      i = best;
+      l = i * 2 + 1;
+    }
+  }
+
+  bool isEmpty() { return heapSize == 0; }
+
+  void swap(int i, int j)
+  {
+    int tmp = heap[i];
+    heap[i] = heap[j];
+    heap[j] = tmp;
+    where[heap[i]] = i;
+    where[heap[j]] = j;
+  }
 };
